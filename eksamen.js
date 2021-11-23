00ce00b410c72f80a80b5b3cab2de50ab605cb1e
sameer_0411@hotmail.com <sameer_0411@hotmail.com>
2021-11-23 14:04:35 +0100

    Start på html fil

--- eksamen.js
+++ eksamen.js
@@ -1,1 +1,16 @@
-console.log
+let btnLogin = document.getElementById("login");
+let btnSignUp = document.getElementById("signup");
+
+let signIn = document.querySelector(".signin");
+let signUp = document.querySelector(".signup");
+
+btnLogin.onclick = function(){
+    signIn.classList.add("active");
+    signUp.classList.add("inActive");
+}
+
+btnSignUp.onclick = function(){
+   signIn.classList.remove("active");
+   signUp.classList.remove("inActive");
+}
+
