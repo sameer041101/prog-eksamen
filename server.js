5673e1d50482053d63517141de62104ec7ed89cf
sameer_0411@hotmail.com <sameer_0411@hotmail.com>
2021-12-10 06:19:12 +0100

    redigering af filer

--- server.js
+++ server.js
@@ -58,9 +58,10 @@ app.get('/homecss', (req, res) => {
 app.get('/image', (req, res) => {
   res.sendFile(__dirname + '/hus.jpeg');
 });
-app.get('/image1', (req, res) => {
+app.get('/image2', (req, res) => {
   res.sendFile(__dirname + '/tøj.jpeg');
 });
+
 // Route to Homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/eksamen.html');
